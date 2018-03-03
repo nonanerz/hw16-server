8518f796de96226f701e2cfcce02b02d9885e2fe
Bulava Eduard <bulavaeduard@gmail.com>
2018-03-03 15:03:07 +0200

    trying to fix validation

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.use((req, res, next) => {
   err.status = 404
   next(err)
 })
-// app.use((error, req, res, next) => {
-//   if (error) {
-//     console.log(error)
-//     return res.status(400).json({error})
-//   }
-//   next(error)
-// })
+app.use((error, req, res, next) => {
+  if (error) {
+    console.log(error)
+    return res.status(400).json({error})
+  }
+  next(error)
+})
 app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.render('error', {
