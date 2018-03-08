06f194e7b33354af8fe86076f5dee996e730ac10
Bulava Eduard <bulavaeduard@gmail.com>
2018-03-08 23:30:36 +0200

    filter by date

--- event/model.js
+++ event/model.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 
 const eventSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String },
   lng: { type: Number, required: true },
   lat: { type: Number, required: true },
-  date: { type: String, required: true },
-  description: { type: String, required: true }
+  date: { type: String },
+  description: { type: String }
 })
 
 module.exports = mongoose.model('Event', eventSchema)
